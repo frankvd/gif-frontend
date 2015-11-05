bc6933152ae7e83126c35f0ab790114e1612b5cd
vrenc <vrencvd@gmail.com>
2015-11-06 00:37:35 +0100

    SQL import

---
+++ sql/import.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `username` TEXT NOT NULL,
+  `password` TEXT NOT NULL,
+  PRIMARY KEY (`username`)
+);
+
+INSERT INTO `user` VALUES('AzureDiamond', '$2y$10$sdlaZ3fg7sutcyFmXKZdzeOK8fdK9dBeeQa2uON0mRRj0gGD.7.6q');
